04e0742facdc991c96e1fc03d88c299f084c342f
Yang Liu <foamliu@yeah.net>
2018-09-26 14:56:15 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -26,9 +26,10 @@ def build_wordmap():
 
     # Create word map
     words = [w for w in word_freq.keys() if word_freq[w] > min_word_freq]
-    word_map = {k: v + 2 for v, k in enumerate(words)}
+    word_map = {k: v + 3 for v, k in enumerate(words)}
     word_map['<start>'] = 0
     word_map['<end>'] = 1
+    word_map['<unk>'] = 2
     print(len(word_map))
     print(words[:10])
 
